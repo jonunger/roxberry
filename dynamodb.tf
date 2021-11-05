9f0e8777638d72750e401afb4cd2b1f59614a814
Jon <9308625+jonunger@users.noreply.github.com>
2021-11-04 23:59:02 -0500

    Update dynamodb.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -27,3 +27,14 @@ resource "aws_dynamodb_table" "equity_price" {
   }
     
 }
+
+
+resource "aws_kinesis_stream" "equity_price" {
+  name        = "equity_price_changes"
+  shard_count = 1
+}
+
+resource "aws_dynamodb_kinesis_streaming_destination" "equity_price" {
+  stream_arn = aws_kinesis_stream.equity_price.arn
+  table_name = aws_dynamodb_table.equity_price.name
+}
